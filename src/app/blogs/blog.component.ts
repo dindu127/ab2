691066e51b8845b3456a72805e14bc1ac87785de
Dinesh127 <veldinesh5@gmail.com>
2026-01-08 19:23:20 +0530

    other links5

--- src/app/blogs/blog.component.ts
+++ src/app/blogs/blog.component.ts
@@ -1,42 +1,46 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { BlogService } from '../services/blog.service';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
-  imports:[CommonModule],
+  standalone: true,
+  imports: [CommonModule,  HttpClientModule],
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  styleUrls: ['./blog.component.scss'],
+  
 })
 export class BlogComponent implements OnInit {
-
-  htmlContent = '';
-blogData: any;
   blog: any;
+  blogData: any;
 
   constructor(
     private route: ActivatedRoute,
-    private http: HttpClient
+    private blogService: BlogService
   ) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe(params => {
       const slug = params.get('slug');
       if (slug) {
-        this.loadStaticBlog(slug);
+        this.loadBlog(slug);
       }
     });
   }
 
-    loadStaticBlog(slug: string): void {
-      const path = `assets/blogs/${slug}.json`;
+  loadBlog(slug: string): void {
+    this.blog = null; // 🔥 IMPORTANT
 
-      this.http.get<any>(path).subscribe({
-        next: data => this.blog = data,
-        error: () => this.blog = null
-      });
-    }
+    this.blogService.getBlogBySlug(slug).subscribe({
+      next: (res) => {
+        this.blog = res;
+      },
+      error: (err) => {
+        console.error('Blog load error', err);
+      }
+    });
+  }
 
 }
